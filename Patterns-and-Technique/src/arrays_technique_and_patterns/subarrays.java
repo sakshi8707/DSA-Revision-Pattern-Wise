fcaddb5f62de39d4ecf72137b2eb4bf12840e8d2
Premjeet Sahu <cs2054@global.org.in>
2025-07-01 23:52:08 +0530

    initial-commit

---
+++ Patterns-and-Technique/src/arrays_technique_and_patterns/subarrays.java
@@ -0,0 +1,8 @@
+package arrays_technique_and_patterns;
+
+public class subarrays {
+    public static void main(String[] args) {
+        //  All the basics of subarrays is present in this file with code implementation
+    }
+
+}
